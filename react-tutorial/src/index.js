42b59e554b2144508bec28647cf2a73b343fedaf
Jenny Lin <misslinyang91@gmail.com>
2021-09-18 13:56:36 +1000

    added onClick event

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -11,28 +11,59 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 //setup vars
-const author = "Lians Moritye";
-const title = "Apples Never Falllll";
-const img =
-  "https://images-na.ssl-images-amazon.com/images/I/91YZYvWIhgL._AC_UL200_SR200,200_.jpg";
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91YZYvWIhgL._AC_UL200_SR200,200_.jpg",
+    title: "Apples Never Falllll",
+    author: "Lians Moritye",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71fWy4UWqlL._AC_UL200_SR200,200_.jpg",
+    title: "The Seven Husbands of Eve Hugo",
+    author: "Taylor Reid",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/61MokdtAKhL._AC_UL200_SR200,200_.jpg",
+    title: "What is poo",
+    author: "Katie Dyanes",
+  },
+];
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book job="woman" />
-      <Book title="superhero" number={22} />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = (props) => {
-  console.log(props);
+const Book = ({ img, title, author }) => {
+  //attribute, event Handler
+  //onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e.target);
+    alert("hello world");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
+  //const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      <p>{props.job}</p>
-      <p>{props.title}</p>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
